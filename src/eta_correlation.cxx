af04aa251c5e26abfbefe2627bf489da76812b62
Carlo Wood <carlo@alinoe.com>
2025-06-03 21:15:46 +0200

    WIP

--- src/eta_correlation.cxx
+++ src/eta_correlation.cxx
@@ -123,16 +123,18 @@ int main(int argc, char* argv[])
 
   // Prepare 1000 buckets to store values in the range [-0.5, 0.5];
   constexpr int number_of_buckets = 1000;
-  Buckets buckets(number_of_buckets, -0.5, 0.5);
-
-  std::uniform_real_distribution dist1_2(1.0, 2.0);
+  Buckets buckets(number_of_buckets, -2.5, 2.5);
 
   constexpr double limit = 13263554;     // 0.5 / sqrt(0.1) * 2^23, where 0.25 < sqrt(0.1) < 0.5 so that floor(log2(limit)) = 23.
   cairowindow::Range const random_x_range{0.999 * limit, limit};
+  std::ostringstream formula;
+
   constexpr int loop_size = 10000000;
   for (int n = 0; n < loop_size; ++n)
   {
     mpreal x = random_mpreal(random_x_range.min(), random_x_range.max(), randstate);
+
+    // d1
     mpreal x2 = utils::square(x);
     // ~ (0.5 / sqrt(0.1) * 2^23)^2 =
     //    0.5^2 / 0.1 * 2^46 =
@@ -140,12 +142,62 @@ int main(int argc, char* argv[])
     //    0.5 / 0.4 * 2^47
     //    when floor(log2(limit^2)) = 47 because 0.25 < 0.4 < 0.5.
     double d1 = x2.toDouble();
-    mpreal eta1 = (mpreal{d1} - x2) / abs(x2);  // Hence we expect this to have a PDF uniform between ±0.4.
+    // d2
+    mpreal c2_x = cubic[2] * x;
+    double d2 = c2_x.toDouble();
+    // d3
+    mpreal c3_x2 = cubic[3] * x2;
+    mpreal c3_d1 = cubic[3] * mpreal{d1};
+    double d3 = c3_d1.toDouble();
+    // d4
+    mpreal c2_x_plus_c3_x2 = c2_x + c3_x2;
+    mpreal d2_plus_d3 = mpreal{d2} + mpreal{d3};
+    double d4 = d2_plus_d3.toDouble();
+    // d5
+    mpreal c1_plus_c2_x_plus_c3_x2 = cubic[1] + c2_x_plus_c3_x2;
+    mpreal c1_plus_d4 = cubic[1] + mpreal{d4};
+    double d5 = c1_plus_d4.toDouble();
+    // d6
+    mpreal c1_x_plus_c2_x2_plus_c3_x3 = c1_plus_c2_x_plus_c3_x2 * x;
+    mpreal d5_x = mpreal{d5} * x;
+    double d6 = d5_x.toDouble();
+    // f_a
+    mpreal c0_plus_c1_x_plus_c2_x2_plus_c3_x3 = cubic[0] + c1_x_plus_c2_x2_plus_c3_x3;
+    mpreal c0_plus_d6 = cubic[0] + mpreal{d6};
+    double f_a = c0_plus_d6.toDouble();
+#define F_A 1
+#if D1
+    mpreal eta = (mpreal{d1} - x2) / abs(x2);  // Hence we expect this to have a PDF uniform between ±0.4.
+    if (n == 0)
+      formula << "((double)d1 - x^2) / x^2";
+#elif D2
+    mpreal eta = (mpreal{d2} - c2_x) / abs(c2_x);
+    if (n == 0)
+      formula << "((double)d2 - c2 x) / |c2 x|";
+#elif D3
+    mpreal eta = (mpreal{d3} - c3_x2) / abs(c3_x2);
+    if (n == 0)
+      formula << "((double)d3 - c3 x^2) / |c3 x^2|";
+#elif D4
+    mpreal eta = (mpreal{d4} - c2_x_plus_c3_x2) / abs(c2_x_plus_c3_x2);
+    if (n == 0)
+      formula << "((double)d4 - (c2 x + c3 x^2)) / |c2 x + c3 x^2|";
+#elif D5
+    mpreal eta = (mpreal{d5} - c1_plus_c2_x_plus_c3_x2) / abs(c1_plus_c2_x_plus_c3_x2);
+    if (n== 0)
+      formula << "((double)d5 - (c1 + c2 x + c3 x^2) / |c1 + c2 x + c3 x^2|";
+#elif D6
+    mpreal eta = (mpreal{d6} - c1_x_plus_c2_x2_plus_c3_x3) / abs(c1_x_plus_c2_x2_plus_c3_x3);
+    if (n == 0)
+      formula << "((double)d6 - (c1 x + c2 x^2 + c3 x^3) / |c1 x + c2 x^2 + c3 x^3|";
+#elif F_A
+    mpreal eta = (mpreal{f_a} - c0_plus_c1_x_plus_c2_x2_plus_c3_x3) / abs(c0_plus_c1_x_plus_c2_x2_plus_c3_x3);
+    if (n == 0)
+      formula << "((double)f_a - (c0 + c1 x + c2 x^2 + c3 x^3) / |c0 + c1 x + c2 x^2 + c3 x^3|";
+#endif
 
-    buckets.insert(eta_to_machine_epsilons(eta1.toDouble()));
+    buckets.insert(eta_to_machine_epsilons(eta.toDouble()));
   }
-  std::ostringstream formula;
-  formula << "((double)d1 - x^2) / abs(x^2)";
 
   std::cout << "Max count: " << buckets.max_count() << '\n';
 
@@ -153,8 +205,9 @@ int main(int argc, char* argv[])
 
   std::ostringstream title;
   title << "η Probability Density Function " << formula.str() << " with x in " << random_x_range;
-  Range const eta_div_epsilon_range{-0.5, 0.5};
-  Range const pdf_range{0, 2.0};
+  double const count_scale = static_cast<double>(number_of_buckets) / (loop_size * buckets.width());
+  Range const eta_div_epsilon_range{buckets.min_value(), buckets.max_value()};
+  Range const pdf_range{0, count_scale * buckets.max_count()};
   QuickGraph qg(title.str(), "η/ε", "P(η | x)", eta_div_epsilon_range, pdf_range);
 
   draw::LineStyle red_line_style({.line_color = color::red, .line_width = 2.0});
@@ -164,7 +217,7 @@ int main(int argc, char* argv[])
   {
     int count = buckets_vector[bucket];
     double eta_x = static_cast<double>(bucket) / buckets_vector.size() * buckets.width() + buckets.min_value();
-    qg.add_line(cairowindow::LinePiece{{eta_x, 0}, {eta_x, number_of_buckets * static_cast<double>(count) / loop_size}});
+    qg.add_line(cairowindow::LinePiece{{eta_x, 0}, {eta_x, count_scale * count}});
   }
 
   // Draw the single-η distribution.
