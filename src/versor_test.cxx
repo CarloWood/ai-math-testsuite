f848eea9d421bd2c65d600c084032b1a5254350c
Carlo Wood <carlo@alinoe.com>
2025-11-27 00:37:20 +0100

    Temporarily change UniverseA to 10 dimensions, and playground7 to 3.

--- src/versor_test.cxx
+++ src/versor_test.cxx
@@ -7,10 +7,10 @@ enum A {};
 struct B {};
 } // namespace universe
 
-using UniverseA = math::Universe<universe::A, 32>;      // Orthonormal basis: { e1, e2, e3, ... e32 }
+using UniverseA = math::Universe<universe::A, 10>;      // Orthonormal basis: { e1, e2, e3, ... e32 }
 using UniverseB = math::Universe<universe::B, 5>;       // Orthonormal basis: { e1, e2, e3, ... e5 }
 
-constexpr UniverseA::subset_type playground7{0x7f};     // Use the seven least significant bits for 'playground7'.
+constexpr int playground7 = 3;                          // Use 7 dimensions for 'playground7'.
 
 int main()
 {
@@ -20,11 +20,10 @@ int main()
   //Similitude sim;
 
   // Construct a 7-dimensional Coordinate-Subspace in UniverseA.
-  using playground7_coordinate_subspace_basis = UniverseA::CoordinateSubspace<playground7>::Basis;
+  using playground7_coordinate_subspace_basis = UniverseA::CoordinateSubspace<playground7>::basis_type;
 
   Dout(dc::notice, "UniverseA::basis_type = " << debug::type_name_of<UniverseA::basis_type>());
   Dout(dc::notice, "playground7_coordinate_subspace_basis = " << debug::type_name_of<playground7_coordinate_subspace_basis>());
-  Dout(dc::notice, "axes = " << std::popcount(UniverseA::CoordinateSubspace<playground7>::axes));
   Dout(dc::notice, "with number of dimensions: " << playground7_coordinate_subspace_basis::n);
 
   Dout(dc::notice, "Leaving main()");
